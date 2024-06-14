c555b52b7dcf0e1a2758ed2480ad33216f12f281
Jamilur Rahman <r.jamilur96@gmail.com>
2024-06-14 02:46:57 -0400

    added timer to splash. Converted all xml to activities

--- app/src/main/java/com/example/deadrobotrestaurant/MainActivity.kt
+++ app/src/main/java/com/example/deadrobotrestaurant/MainActivity.kt
@@ -9,19 +9,20 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.enableEdgeToEdge
 import com.example.deadrobotrestaurant.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        }
-
     }
+
+}
