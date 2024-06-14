8902e58eb8f79665512e7ec4a97681debfc590a1
Jamilur Rahman <r.jamilur96@gmail.com>
2024-06-14 02:54:22 -0400

    changes to xmls to activities and added splash timer

---
+++ app/src/main/java/com/example/deadrobotrestaurant/ProductDetailActivity.kt
@@ -0,0 +1,29 @@
+package com.example.deadrobotrestaurant
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.deadrobotrestaurant.databinding.ActivityProductDetailBinding
+
+class ProductDetailActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityProductDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityProductDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+        }
+    }
+
---
+++ app/src/main/java/com/example/deadrobotrestaurant/SplashActivity.kt
@@ -0,0 +1,36 @@
+package com.example.deadrobotrestaurant
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.deadrobotrestaurant.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000) // auto redirect to main activity
+
+    }
+
+}
