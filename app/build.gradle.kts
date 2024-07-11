c944415228e0eaba1d29bae99e3b33650924e7de
Jamilur Rahman <r.jamilur96@gmail.com>
2024-07-11 00:54:04 -0400

    register activity functioning, TODO store users and navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
 
 dependencies {
 
+    implementation(libs.androidx.activity)
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
